390b7b4664e2261323830bfe7fdbca1c0e33ffa1
hamcam98 <hamcalum10@gmail.com>
2021-02-19 12:20:41 +0000

    Run_agents working

--- final-agents/run_agents.py
+++ final-agents/run_agents.py
@@ -8,23 +8,25 @@ from TD3 import td3_run
 from PPO import ppo_run
 from SAC import sac_run
 
-continuous_envs = ['LunarLanderContinuous-v2', 'MountainCarContinuous-v0',
-                    'InvertedPendulumBulletEnv-v0'] #Hopper-v2
-continuous_env_steps = [50000, 50000,
-                        50000]
+number_of_continuous_runs = 1
+continuous_envs = ['LunarLanderContinuous-v2', 'MountainCarContinuous-v0', 'InvertedPendulumBulletEnv-v0'] #Hopper-v2
+continuous_env_steps = [50000, 50000, 50000]
 continuous_runs = [ddpg_run, td3_run, sac_run] #ppo_run, 
-continous_action_spaces = [2, 2, 1]
+continuous_action_spaces = [2, 2, 1]
 continuous_obs_spaces = [(8,), (4,), (5,)]
 
+number_of_discrete_runs = 1
 discrete_envs = ['LunarLander-v2', 'MountainCar-v0']
 discrete_env_steps = [50000, 50000]
 discrete_runs = [dqn_run]
 
 if __name__ == '__main__':
     env_steps_continuous = list(zip(continuous_action_spaces, continuous_obs_spaces, continuous_envs, continuous_env_steps))
-    for idx, run in enumerate(continuous_runs):
-        run(actions=env_steps_continuous[idx][0], obs=env_steps_continuous[idx][1],
-            env_id=env_steps_continuous[idx][2], total_runs=env_steps_continuous[idx][3], run=idx)
-
-    for idx, run in enumerate(discrete_runs):
-        run(env_id=env_steps_continuous[idx][0], total_runs=env_steps_continuous[idx][1])
+    for times in range(number_of_continuous_runs):
+        for idx, run in enumerate(continuous_runs):
+            run(actions=env_steps_continuous[idx][0], obs=env_steps_continuous[idx][1],
+                env_id=env_steps_continuous[idx][2], total_games=env_steps_continuous[idx][3], run=idx)
+    
+    for times in number_of_discrete_runs:
+        for idx, run in enumerate(discrete_runs):
+            run(env_id=env_steps_continuous[idx][0], total_games=env_steps_continuous[idx][1])
