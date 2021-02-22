f401958d71124061887e7d231e526f9da6754373
Calum <calum.hamilton@outlook.com>
2021-02-22 11:28:47 +0000

    DDPG Run push

--- final-agents/run_agents.py
+++ final-agents/run_agents.py
@@ -10,14 +10,14 @@ from SAC import sac_run
 
 number_of_continuous_runs = 1
 continuous_envs = ['LunarLanderContinuous-v2', 'MountainCarContinuous-v0', 'InvertedPendulumBulletEnv-v0'] #Hopper-v2
-continuous_env_steps = [50000, 50000, 50000]
-continuous_runs = [ddpg_run, td3_run, sac_run] #ppo_run, 
+continuous_env_steps = [20000, 20000, 20000]
+continuous_runs = [ddpg_run, td3_run, sac_run] #ppo_run,
 continuous_action_spaces = [2, 1, 1]
 continuous_obs_spaces = [(8,), (2,), (5,)]
 
 number_of_discrete_runs = 1
 discrete_envs = ['LunarLander-v2', 'MountainCar-v0']
-discrete_env_steps = [50000, 50000]
+discrete_env_steps = [20000, 20000]
 discrete_runs = [dqn_run]
 
 if __name__ == '__main__':
@@ -26,7 +26,7 @@ if __name__ == '__main__':
         for idx, run in enumerate(continuous_runs):
             run(actions=env_steps_continuous[idx][0], obs=env_steps_continuous[idx][1],
                 env_id=env_steps_continuous[idx][2], total_games=env_steps_continuous[idx][3], run=idx)
-    
+
     for times in number_of_discrete_runs:
         for idx, run in enumerate(discrete_runs):
             run(env_id=env_steps_continuous[idx][0], total_games=env_steps_continuous[idx][1])
